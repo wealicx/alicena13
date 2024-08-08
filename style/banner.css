63845fa8519152d5a21f602946e45bbef78ba318
wealicx <144695058+wealicx@users.noreply.github.com>
2024-08-08 19:53:03 -0300

    eundamid

---
+++ style/banner.css
@@ -0,0 +1,32 @@
+.banner{
+    background: var(--azul-degrade);
+    color: var(--branco);
+    text-align: center;
+    padding: 2.5em 0;
+}
+.banner__titulo{
+    font-size: 16px;
+    font-weight: 700;  
+}
+.banner__text{
+    font-weight: 500;
+    font-size: 14px;
+    margin: 3em 0;
+}
+.banner__pesquisa{
+    background-color: transparent;
+    border: 1px solid var(--branco);
+    color: var(--branco);
+    border-radius: 24px;
+    padding: 2em 0;
+    width: 100%;
+   
+.banner__pesquisa::placeholder{
+    font-family: var(--fonte-principal);
+    font-size: 14px;
+    font-weight: 500;
+    text-align: center;
+    color: var(--branco);
+    background: url("../img/Lupa.svg") no-repeat;
+    background-position: 1em;
+}
